e356e42b3c1b19bd98f7a1058a02b5e2edf63d1a
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-02-16 05:53:56 +0200

    Copying Arrays

---
+++ 56. Copying Arrays/56. Copying Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class ArrayCopyingExample
+{
+    static void Main()
+    {
+        // Original array
+        int[] original = { 1, 2, 3, 4, 5 };
+
+
+        // Array to hold the copy
+        int[] copy = new int[5];
+
+
+        // Copying array
+        Array.Copy(original, copy, original.Length);
+
+
+        // Displaying the copied array
+        Console.WriteLine("Copied Array:");
+        foreach (int element in copy)
+        {
+            Console.WriteLine(element);
+        }
+        Console.ReadKey();
+    }
